9fac31354c292279e6dcde30ad208d7d2a1f708b
Tifemax <boluayo99@gmail.com>
2024-09-04 15:17:31 +0100

    "Updated Footer component with new layout, styles, and added FaCircleDot icon"

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,4 @@
+import { FaCircleDot } from "react-icons/fa6";
 import { TbBuildingStore, TbHandLoveYou } from "react-icons/tb";
 import { Link } from "react-router-dom";
 
@@ -6,34 +7,32 @@ const Footer = () => {
       <footer
         style={{
           backgroundColor: "black",
-          color: "white",
-          
-          textAlign: "center",
-          width: "100%",
+          color: 'whitesmoke'
         }}
       >
-        {/* <h1>React App</h1>
-        <p>&copy; TifeMaxi</p> */}
-        <div className="container-fluid d-flex">
-          <div style={{ marginTop: '60px'}}>
-            <h2>Fake Store App <TbBuildingStore /> </h2>
-            <h4>Made with <TbHandLoveYou /> by Tife</h4>
-          </div>
-          <div style={{ marginLeft: '250px'}}>
-            <h1><b>Categories :</b></h1>
+       
+        <div className="footer_cointainer">
+           <div className="row">
+            <div className="col-lg-4 col-xl-4" style={{marginTop: '20px'}}>
+            <h2><b>Fake Store App</b> <TbBuildingStore /> </h2>
+            </div>
+            <div className="col-lg-4 col-lg-3" style={{marginTop: '20px'}}>
+            <h1 style={{color: 'red'}}><b>Categories :</b></h1>
             <p><b>Electronics</b></p>
             <p><b>Jewerely</b></p>
             <p><b>MensClothing</b></p>
             <p><b>WomansClothing</b></p>
-          </div>
-          <div style={{ marginLeft: '200px', marginTop: '60px', overflow: 'hidden'}}>
+            </div>
+
+            <div className="col-lg-4 col-xl-3" style={{marginTop: '20px'}}>
+            <p>Subscribe to our newletter to stay Updated</p>
             <input type="text" placeholder="Email" style={{borderRadius: '50px', border: '1px solid yellow'}}></input>
             <button type="button" class="btn btn-outline-danger">Subscribe</button>
-          </div>
-           
-           
-          
-        
+            </div>
+           </div>
+        </div>
+        <div className="footer_bottom" style={{backgroundColor: 'lightblack', textAlign: 'center'}}>
+         <p><FaCircleDot />A Fake Store App <FaCircleDot />  Made by Tife</p>
         </div>
       </footer>
     );
